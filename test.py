5f320fa571e5a9c1bebade989864004f6e6b8475
Kaushik Ram Ganapathy <krganapa@ucsd.edu>
2021-05-12 17:49:07 +0530

    Finalizing Airavata Scripts

--- test.py
+++ test.py
@@ -28,8 +28,8 @@ import os
 
 
 
-map_path = "/Users/kaushikramganapathy/Downloads/temp_project/layouts/schoollayout1/schoollayout1_processed.shp" #"/home/geoact/layouts/schoollayout1.shp" #"./test/testdata/schoollayout1.shp"
-schedule_path = "/Users/kaushikramganapathy/testschoolmodel/0527291751/schedule_data/day_schedule.csv" 
+map_path = "/home/geoact/schoollayout1/schoollayout1_processed.shp"#"/Users/kaushikramganapathy/Downloads/temp_project/layouts/schoollayout1/schoollayout1_processed.shp" #"/home/geoact/layouts/schoollayout1.shp" #"./test/testdata/schoollayout1.shp"
+schedule_path = "/home/geoact/schedule_data/day_schedule.csv"#"/Users/kaushikramganapathy/testschoolmodel/0527291751/schedule_data/day_schedule.csv" 
 schedule_steps = 90 # full day_schedule steps should be 90
 
 # Trying to get this out of config files
